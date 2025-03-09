0a1baec4b134259859b8acb108f3f1a6603ba383
Hina Nasir <hina.nasir@warwick.ac.uk>
2025-03-09 19:53:17 +0000

    incomplete code on merge and quick sort worksheet

---
+++ Merge_quick_sort.py
@@ -0,0 +1,105 @@
+import heapq
+
+# Creating a list
+li = [10, 20, 15, 30, 40]
+
+# Convert the list into a heap
+heapq.heapify(li)
+
+print("Heap queue:", li)
+
+
+
+def partition(array, low, high):
+
+    # choose the rightmost element as pivot
+    pivot = array[high]
+
+    # pointer for greater element
+    i = low - 1
+    for j in range(low, high):
+        if array[j] <= pivot:
+            i = i + 1
+            (array[i], array[j]) = (array[j], array[i])
+
+    (array[i + 1], array[high]) = (array[high], array[i + 1])
+    return i + 1
+
+
+def quickSort(array, low, high):
+    if low < high:
+        pi = partition(array, low, high)
+        quickSort(array, low, pi - 1)
+        quickSort(array, pi + 1, high)
+
+
+def merge(arr, left, mid, right):
+    n1 = mid - left + 1
+    n2 = right - mid
+
+    # Create temp arrays
+    L = [0] * n1
+    R = [0] * n2
+
+    # Copy data to temp arrays L[] and R[]
+    for i in range(n1):
+        L[i] = arr[left + i]
+    for j in range(n2):
+        R[j] = arr[mid + 1 + j]
+
+    i = 0  # Initial index of first subarray
+    j = 0  # Initial index of second subarray
+    k = left  # Initial index of merged subarray
+
+    # Merge the temp arrays back
+    # into arr[left..right]
+    while i < n1 and j < n2:
+        if L[i] <= R[j]:
+            arr[k] = L[i]
+            i += 1
+        else:
+            arr[k] = R[j]
+            j += 1
+        k += 1
+
+    # Copy the remaining elements of L[],
+    # if there are any
+    while i < n1:
+        arr[k] = L[i]
+        i += 1
+        k += 1
+
+    # Copy the remaining elements of R[],
+    # if there are any
+    while j < n2:
+        arr[k] = R[j]
+        j += 1
+        k += 1
+
+def merge_sort(arr, left, right):
+    if left < right:
+        mid = (left + right) // 2
+
+        merge_sort(arr, left, mid)
+        merge_sort(arr, mid + 1, right)
+        merge(arr, left, mid, right)
+
+def print_list(arr):
+    for i in arr:
+        print(i, end=" ")
+    print()
+
+# Driver code
+arr = [12, 11, 13, 5, 6, 7]
+print("Given array is")
+print_list(arr)
+
+merge_sort(arr, 0, len(arr) - 1)
+size = len(arr)
+print("\nSorted array using merge sort")
+print_list(arr)
+quickSort(arr, 0, size - 1)
+
+
+print('Sorted Array using quick sort')
+print(arr)
